5690de713f586fcebdc5721fb9bc4bae23bd27c5
mondigrooming-shop <mondigrooming.shop@gmail.com>
2026-07-26 01:02:45 -0400

    Trigger redeploy with updated product data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import'next'].NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -6,6 +6,9 @@ const nextConfig = {
       { protocol: "https", hostname: "eaas01-ga.myshopify.com" },
     ],
   },
+  // Force dynamic rendering for fresh product data
+  experimental: {},
 };
 
 module.exports = nextConfig;
+
